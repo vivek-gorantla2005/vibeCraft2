68342cb0d636c284cc519783ed7df97f5ee80b62
vivekgorantla <23eg105j66@anurag.edu.in>
2025-08-16 19:17:12 +0530

    added newproject-input/feature

--- src/app/components/NewProject.jsx
+++ src/app/components/NewProject.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "@/components/ui/spotlight";
-import { NewPojectButton } from "./NewProjectButton";
+import { NewProjectButton } from "./NewProjectButton";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 export function NewProjectSpotlight() {
@@ -40,7 +40,7 @@ export function NewProjectSpotlight() {
           copy.
         </p>
         <div className="mt-1">
-          <NewPojectButton />
+          <NewProjectButton/>
         </div>
       </div>
     </div>
